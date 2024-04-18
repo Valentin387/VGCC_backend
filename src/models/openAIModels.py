44c461877e49ea07a03843c096585ae943bf3ebc
Antoine Boucher <antoine.bou13@gmail.com>
2024-04-18 10:04:45 -0400

    Update openAIModels.py

--- src/models/openAIModels.py
+++ src/models/openAIModels.py
@@ -1,4 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import List, Optional
+
+class EventCreateInput(BaseModel):
+    summary: str
+    location: Optional[str] = None
+    description: Optional[str] = None
+    start: str  # RFC3339 datetime
+    end: str    # RFC3339 datetime
+    attendees: Optional[List[EmailStr]] = []
+    reminders: Optional[List[dict]] = []
 
 class InputText(BaseModel):
     input_text: str
