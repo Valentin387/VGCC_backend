5709794a0447cda6200d1069443dca0ce6c8b142
Antoine Boucher <antoine.bou13@gmail.com>
2024-04-18 10:03:42 -0400

    Update openai_router.py

--- src/routers/openai_router.py
+++ src/routers/openai_router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from ..services.openai_service import get_llm_response, handle_text_deletion, handle_text_creation, handle_text_append
+from models.openAIModels import InputText, EventCreateInput
+from services.openai_service import (get_llm_response, handle_text_deletion, handle_text_creation,
+                                     handle_text_append, interpret_and_schedule_event)
 
 router = APIRouter()
 
@@ -19,6 +21,9 @@ async def create_text():
 async def append_text(input_data: InputText):
     return await handle_text_append(input_data)
 
-@router.post("/create-event/")
-async def create_event(event_data: EventCreateInput):
-    return await schedule_event(event_data)
+@router.post("/schedule-event/")
+async def schedule_event(input_text_data: InputText):
+    """
+    Endpoint to schedule a new event based on natural language input.
+    """
+    return await interpret_and_schedule_event(input_text_data)
